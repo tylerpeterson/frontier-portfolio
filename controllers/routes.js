bbe24886319318a4b915d09bed34042020b7415f
Tyler Peterson <petersontb@familysearch.org>
2013-08-15 11:39:01 -0600

    Implement Frontier Apprentice
    Add another route and custom content.
    Pass data to an ejs template.
    Create a simple CRUD capable list using Angular.
    Create and use a custom layout.

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,11 +1,13 @@
 module.exports = function(app) {
-  
+
   /* Optionally include this to use this alternative template when wanting to create your own header and footer
   app.set("view options", {
     layout: "layout/layout-skills"
   });
   */
-  
+  app.get('/angular', function (req, res) {
+    res.render('angular', {layout: 'layout/layout_angello', host: req.headers.host});
+  });
   app.get('/:page', function(req, res){
     res.render(req.params.page, {});
   });
